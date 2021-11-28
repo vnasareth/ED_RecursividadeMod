b25523fec49df0660c4bb6695d3ad95bfc8983fb
vnasa <vnasa@LAPTOP-VETU9ACB>
2021-11-28 20:58:08 -0300

    one commit

--- Controller/DivisaoController.java
+++ Controller/DivisaoController.java
@@ -3,7 +3,32 @@ package Controller;
 public class DivisaoController {
 
 	public DivisaoController() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
-}
+	public int div(int dividendo,int divisor,int resultado,int resto,int incremento) {
+		
+		//Condicao de parada
+		// linha de if o resultado for igual a 0 retornar 0
+		if (resultado == 0 ) {
+		 return 0;
+			
+		//se for diferente de 0 faça
+	} else {	
+	
+		// resultado vai ser dividendo / divisor
+		resultado = dividendo / divisor;
+		
+		resto = dividendo % divisor ;
+		
+		// se o resto for igual a 9 conta + 1
+		if (resto == 9) {
+		incremento = incremento + 1;
+		return incremento + div(resultado,9);
+		 
+		}
+	}
+
+		}
+	}
+	
--- view/Principal.java
+++ view/Principal.java
@@ -1,10 +1,25 @@
 package view;
 
+import javax.swing.JOptionPane;
+
+import Controller.DivisaoController;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		DivisaoController dc = new DivisaoController();
+		
+		int dividendo = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o numero desejado de 0 ate 999999: "));
+		int divisor = 20;
+		int resultado = 0 ;
+		int resto = 5 ;
+		int incremento = 0;
+		
+		int div = dc.div(dividendo,divisor,resultado,resto,incremento);
+		System.out.println(div);		
+		JOptionPane.showMessageDialog(null, div);
+		
 	}
 
 }
